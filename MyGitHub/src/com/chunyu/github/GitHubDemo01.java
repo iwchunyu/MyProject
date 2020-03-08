bf744a58c9588850794efadea3ce9c88d510e973
iwchunyu <iwchunyu@163.com>
2020-03-08 13:34:15 +0800

    hi  it's me,ggg

--- MyGitHub/src/com/chunyu/github/GitHubDemo01.java
+++ MyGitHub/src/com/chunyu/github/GitHubDemo01.java
@@ -3,5 +3,6 @@ package com.chunyu.github;
 public class GitHubDemo01 {
     public static void main(String[] args) {
         System.out.println("hi 这是我的github项目 你们好啊");
+        System.out.println("cap");
     }
 }
